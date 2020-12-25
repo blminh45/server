76b234645f5d0636dcd22256485ef807ff2b5bb4
blminh <0306181245@caothang.edu.vn>
2020-12-25 10:33:20 +0700

    update

--- api.php
+++ api.php
@@ -1,15 +1,37 @@
 <?php 
 require_once('lib/db.php');
-// $r=DP::run_query('SELECT `id`, `NoiDung`, `MucDo`, `DapAn1`, `DapAn2`, `DapAn3`, `DapAn4`, `DA_Dung` FROM `question` WHERE `MucDo`=?',[$_GET['DoKho']],2);
+
+//lay ra toi da 5 tu
 if(isset($_GET['get'])){
-    $r=DP::run_query('SELECT id, word, definition FROM wordlist', [$_GET['get']], 2);
+    $r=DP::run_query('SELECT id, word, definition, image FROM wordlist limit 5', [$_GET['get']], 2);
     echo json_encode($r);
+    return;
 }
 
-if(isset($_GET['word']) && isset($_GET['definition'])){
-    $i=DP::run_query('INSERT INTO wordlist(word, definition) VALUES (?,?)', [$_GET['word'], $_GET['definition']], 3);
+if(isset($_GET['word']) && isset($_GET['definition']) && isset($_GET['img'])){
+    $i=DP::run_query('INSERT INTO wordlist(word, definition, image) VALUES (?,?,?)', [$_GET['word'], $_GET['definition'], $_GET['img']], 3);
     $result= array("code"=>"200");
     echo json_encode($result);
+    return;
+}
+
+//lay ra toi da 5 tu voi tu khoa bat dau = word
+if(isset($_GET['word'])){
+    $i=DP::run_query("SELECT id, word, definition FROM wordlist WHERE word like CONCAT(?, '%') limit 0,5", [$_GET['word']], 2);
+    echo json_encode($i);
+    return;
+}
+
+function updateImg(){
+    // receive image as POST Parameter
+    $image = str_replace('data:image/png;base64,', '', $_POST['image']);
+    $image = str_replace(' ', '+', $image);
+    // Decode the Base64 encoded Image
+    $data = base64_decode($image);
+    // Create Image path with Image name and Extension
+    $file = '../images/' . "MyImage" . '.jpg';
+    // Save Image in the Image Directory
+    $success = file_put_contents($file, $data);
 }
 
 ?>
